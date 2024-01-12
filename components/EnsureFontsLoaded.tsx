2028399b7be84814a2a4433069d1c4091c01face
Andrew Shapro <4075481+abramz@users.noreply.github.com>
2024-01-12 13:11:15 -0800

    load fontawesome5

--- components/EnsureFontsLoaded.tsx
+++ components/EnsureFontsLoaded.tsx
@@ -1,11 +1,13 @@
 import { useFonts } from "expo-font";
 import { PropsWithChildren, ReactNode, useEffect } from "react";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function EnsureFontsLoaded({
   children,
 }: PropsWithChildren): ReactNode {
   const [loaded, error] = useFonts({
     TitanOne: require("../assets/fonts/TitanOne-Regular.ttf"),
+    ...FontAwesome5.font,
   });
 
   useEffect(() => {
